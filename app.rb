15b434ab553e0dd1fceac14c21ff8abf5934113e
hedudelgado <hedu.delgado@gmail.com>
2016-02-04 18:24:48 +0000

    attacking player 2 finished

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
 
 	enable :sessions
@@ -18,6 +19,12 @@ class Battle < Sinatra::Base
    		@player_1_name = session[:player_1_name]
   		@player_2_name = session[:player_2_name] 
   		erb(:play) 
+  	end
+
+  	get '/attack' do
+   		@player_1_name = session[:player_1_name]
+  		@player_2_name = session[:player_2_name] 
+  		erb :attack 
   	end
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,29 @@
+require 'capybara/dsl'
+require 'selenium-webdriver'
+
+	def browser_view
+ 		include Capybara::DSL
+ 		Capybara.default_driver = :selenium
+	end
 feature 'Enter names' do 
 	scenario 'submitting names' do 
-		visit('/')
-		fill_in :player_1_name, with: 'Dave'
-		fill_in :player_2_name, with: 'Mittens'
-		click_button 'Submit'
+		sign_in_and_play
 		expect(page).to have_content "Dave vs. Mittens"
 	end
 end
+
+feature 'View hitting points' do 
+	scenario'see player 2 hit points' do 
+		sign_in_and_play
+		expect(page).to have_content 'Mittens: 60HP'
+	end
+end
+
+feature 'attack player two' do 
+	scenario'see attack confirmed' do
+		browser_view
+	sign_in_and_play
+	expect(page).to have_content 'Dave attacked Mittens'
+	end
+end 
+
