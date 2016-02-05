f8e5eeac3091503d22b29db7acee61d89d0de96e
hedudelgado <hedu.delgado@gmail.com>
2016-02-05 12:18:48 +0000

    26 done

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,20 @@
 require 'game'
 
 describe Game do 
+	#et(:game)    {double Game.new}
+	let(:dave) 	  {double :dave}
 	let(:mittens) {double :mittens}
+	subject(:game){described_class.new(:dave, :mittens)}
 
-describe '#attack' do
-    	it 'damages the player' do
-      		expect(mittens).to receive(:receive_damage)
-      		subject.attack(mittens)
-   		end
+	it'accepts two instances' do 
+		expect(subject.player1).to eq :dave
+	end
+
+	describe '#attack' do
+    		it 'damages the player' do
+      			expect(mittens).to receive(:receive_damage)
+      			subject.attack(mittens)
+   			end
   	end
+
 end
